73ffe3266dbb3abe4ca81965b90bdc530c240e69
tapasrm <tmohanta9@gmail.com>
2023-06-18 14:44:58 +0530

    Handled errors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::{process::{Command, Stdio, Child}, io::{stdin, stdout, Write}, path::Path, env};
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process::{Child, Command, Stdio},
+};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     loop {
         print!("> ");
-        stdout().flush();
+        stdout().flush().expect("clear output failed");
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
@@ -16,55 +21,51 @@ fn main() {
         while let Some(command) = commands.next() {
             let mut parts: std::str::SplitWhitespace<'_> = command.trim().split_whitespace();
             // everything after the first whitespace is considered as argument.
-        let command = parts.next().unwrap();
-        let args= parts;
+            let command = parts.next().unwrap();
+            let args = parts;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
-                }
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
 
-                previous_command = None;
-            },
-            "exit" => return,
-            command => {
-
-                let stdin = previous_command
-                                .map_or(
-                                    Stdio::inherit(),
-                                    |output: Child| Stdio::from(output.stdout.unwrap())
-                                );
-                
-                let stdout = if commands.peek().is_some() {
-                    // there is another command piped behind this one
-                    // prepare to send output to the next command
-                    Stdio::piped()
-                } else {
-                    // there are no more commands piped behind this one
-                    // send output to shell stdout
-                    Stdio::inherit()
-                };
-
-                let mut command = Command::new(command);
-                if let Ok(mut child) = command.args(args).stdin(stdin).stdout(stdout).spawn() {
-                    child.wait().expect("command wasn't running");
-                    previous_command = Some(child);
-                    println!("Child has finished its execution!");
-                } else {
-                    eprintln!("Previous Command Failed");
                     previous_command = None;
-                };
+                }
+                "exit" => return Ok(()),
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        // there is another command piped behind this one
+                        // prepare to send output to the next command
+                        Stdio::piped()
+                    } else {
+                        // there are no more commands piped behind this one
+                        // send output to shell stdout
+                        Stdio::inherit()
+                    };
+
+                    let mut command = Command::new(command);
+                    if let Ok(mut child) = command.args(args).stdin(stdin).stdout(stdout).spawn() {
+                        child.wait().expect("command wasn't running");
+                        previous_command = Some(child);
+                        println!("Child has finished its execution!");
+                    } else {
+                        eprintln!("Previous Command Failed");
+                        previous_command = None;
+                    };
+                }
             }
-        }
         }
 
         if let Some(mut final_command) = previous_command {
             // block until the final command has finished
-            final_command.wait();
+            final_command.wait().expect("Command failed");
         }
-
     }
 }
